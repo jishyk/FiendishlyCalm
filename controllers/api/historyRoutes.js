b753c3022a30311833a5da206dc27847a10710ad
Domitius <133902698+recenasu@users.noreply.github.com>
2023-09-29 14:08:50 -0700

    debugging routes, still debugging the comment POST

--- controllers/api/historyRoutes.js
+++ controllers/api/historyRoutes.js
@@ -88,7 +88,7 @@ router.delete('/:id', async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
 
 
 module.exports = router;
---
+++ public/js/technique.js
@@ -0,0 +1,28 @@
+// Assign HTML elements to variables
+// const comment = document.querySelector('.comment-textarea');
+// const techIdElement = document.getElementById('tech-id');
+// const technique_id = techIdElement.dataset.id
+
+
+
+const handleCommentSave = async () => {
+    
+    console.log("click!");
+    
+    const payload = {
+        comment: document.querySelector('.comment-textarea').value,
+        technique_id: document.getElementById('tech-id').dataset.id
+        }
+
+        console.log(payload);
+
+        await fetch('/api/history', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    })
+};
+
+document.getElementById('save-button').addEventListener('click', handleCommentSave);
